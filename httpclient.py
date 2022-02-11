a74acbe431f865e382698d5ded2b75f2d8c8382a
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-02-11 12:08:57 -0700

    Implement GET

--- httpclient.py
+++ httpclient.py
@@ -21,6 +21,8 @@
 import sys
 import socket
 import re
+from typing import Tuple
+from urllib import request
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -36,60 +38,133 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        return None
+        self.socket = socket.create_connection((host, port))                    
+        return None    
 
-    def get_code(self, data):
-        return None
+    def get_url(self, url: str) -> Tuple[str, str, int]:
+        parsedUrl = urllib.parse.urlparse(url)
+        # print(parsedUrl.hostname, parsedUrl.path, parsedUrl.port)
+        return(
+            parsedUrl.hostname, 
+            parsedUrl.path if parsedUrl.path != '' else '/', 
+            parsedUrl.port or 80)
 
-    def get_headers(self,data):
-        return None
+    def get_request(self, operation : str, host : str, path : str, y) -> str:
+        request = f"{operation} {path} HTTP/1.1\r\n{self.get_request_headers(host, len(y))}\r\n\r\n"        
 
-    def get_body(self, data):
-        return None
+        print (request)
+        return request
+
+    def get_request_headers(self, host, contentLength = 0) -> str:
+        requestHeaders = {               
+                'Host' : host,
+                'Connection' : 'close',
+                'User-Agent' : 'rkeele/1.0.0', 
+                'Accept' : '*/*'                
+            }
+            
+        if contentLength > 0 :
+            requestHeaders['ContentLength', str(contentLength)]
+
+        return '\r\n'.join(': '.join(x) for x in requestHeaders.items())    
+
+    def get_code(self, data: bytearray) -> int:        
+        return int(data[:data.index(b'\r\n')].decode('utf-8').split(' ')[1])
+
+    def get_headers(self, data: bytearray) -> str:
+        return str.strip(data[:data.index(b'\r\n\r\n')].decode('utf-8'))
+
+    def get_body(self, data: bytearray) -> str:                        
+        return str.strip(data[data.index(b'\r\n\r\n'):].decode('utf-8', 'ignore'))
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
     def close(self):
-        self.socket.close()
+        try:
+            self.socket.close()
+        except: pass            
 
     # read everything from the socket
-    def recvall(self, sock):
-        buffer = bytearray()
-        done = False
-        while not done:
-            part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
-        return buffer.decode('utf-8')
+    def recvall(self, sock) -> str:
+        buffer = bytearray()        
+        while True:
+            part = sock.recv(1024)                   
+            buffer += part
+            if (not part):
+                break        
+        return buffer.decode('utf-8', 'ignore')
+
+    # read everything from the socket
+    def recvall_b(self, sock) -> bytearray:
+        buffer = bytearray()        
+        while True:
+            part = sock.recv(1024)                   
+            buffer += part
+            if (not part):                
+                return buffer                    
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        try:
+            host, path, port = self.get_url(url)          
+            self.connect(host, port)                                
+
+            request = self.get_request('GET', host, path, bytearray())            
+            self.sendall(request)                    
+            response = self.recvall_b(self.socket)                                           
+
+            code = self.get_code(response)
+            headers = self.get_headers(response)
+            body = self.get_body(response)                    
+
+            return HTTPResponse(code, body)
+        # except:
+        #     pass
+        finally:
+            self.close()
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        try:
+            host, path, port = self.get_url(url)          
+            self.connect(host, port)                                
+
+            request = self.get_request('GET', host, path, bytearray())            
+            self.sendall(request)                    
+            response = self.recvall_b(self.socket)                                           
+
+            code = self.get_code(response)
+            headers = self.get_headers(response)
+            body = self.get_body(response)
+            
+            # print(headers, '\n')
+            # print(body, '\n')
+
+            return HTTPResponse(code, body)
+        # except:
+        #     pass
+        finally:
+            self.close()
 
     def command(self, url, command="GET", args=None):
-        if (command == "POST"):
-            return self.POST( url, args )
+        if (command == "GET"):
+            
+            return self.GET(url, args).body
+
+        elif (command == "POST"):
+            
+            return self.POST(url, args)        
+
         else:
-            return self.GET( url, args )
+            return "Unsupported operation."
     
 if __name__ == "__main__":
-    client = HTTPClient()
-    command = "GET"
+    client = HTTPClient()    
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
         print(client.command( sys.argv[2], sys.argv[1] ))
+        # client.command(sys.argv[2], sys.argv[1])
     else:
         print(client.command( sys.argv[1] ))
+        # client.command(sys.argv[1])
